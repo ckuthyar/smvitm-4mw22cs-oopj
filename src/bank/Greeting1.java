31ca12761b9130c04b4395c3a0f3831fda9a2360
Chandrashekar Kuthyar <ckuthyar@gmail.com>
2023-12-06 13:44:47 +0530

    greeting(name,gender). If M - Welcome Mr. Aditya, If F - Welcome Ms Asha

--- src/bank/Greeting1.java
+++ src/bank/Greeting1.java
@@ -7,10 +7,23 @@ public class Greeting1 {
 	public static String greeting(String name) {
 		return "Hello "+name;
 	}
+	public static String greeting(String name, String gender) {
+		String prefix=". ";
+		if(gender=="M") {
+			prefix="Mr";
+		}
+		if(gender=="F") {
+			prefix="Ms";
+		}
+		return "Hello "+prefix+ " "+name;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(greeting());
 		System.out.println(greeting("Kuthyar"));
+		System.out.println(greeting("Aditya","M"));
+		System.out.println(greeting("Asha","F"));
+		
 		
 
 	}
