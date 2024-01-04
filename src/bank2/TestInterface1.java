988deb4ca8df40aac36bd8faed0de21e768c4d38
Chandrashekar Kuthyar <ckuthyar@gmail.com>
2024-01-04 13:40:13 +0530

    TestInterface2 shows extends from class implements interface

---
+++ src/bank2/TestInterface1.java
@@ -0,0 +1,10 @@
+package bank2;
+
+interface HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("This is an interface");
+
+	}
+
+}
---
+++ src/bank2/TestInterface2.java
@@ -0,0 +1,10 @@
+package bank2;
+
+public class TestInterface2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
