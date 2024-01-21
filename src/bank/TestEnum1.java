4b0c02872c8966a553295126ede7d659188759e2
Chandrashekar Kuthyar <ckuthyar@gmail.com>
2024-01-21 11:42:52 +0530

    enum Day of Week Mon,Tue

---
+++ src/bank/TestEnum1.java
@@ -0,0 +1,11 @@
+package bank;
+enum DayOfWeek{
+	MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY,};
+public class TestEnum1 {
+		public static void main(String[] args) {
+		DayOfWeek mon=DayOfWeek.MONDAY;
+		DayOfWeek sun=DayOfWeek.SUNDAY;
+		System.out.println(mon + " is a holiday due to Ayodhya temple inauguration");
+	}
+
+}
